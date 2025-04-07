81f082ef1fecde5774d3a76e4298fe9c42d73598
NolramD <marlondujj@gmail.com>
2025-04-07 10:14:18 +0200

    endpoints apirestcontroller

--- tp4/src/main/java/fr/ensai/running/controller/ApiRestController.java
+++ tp4/src/main/java/fr/ensai/running/controller/ApiRestController.java
@@ -3,7 +3,13 @@ package fr.ensai.running.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,4 +32,31 @@ public class ApiRestController {
         return athleteService.findAll();
     }
 
+    /**
+     * Get an athlete by id
+     */
+    @GetMapping("/athlete/{id}")
+    public ResponseEntity<Athlete> getAthleteById(@PathVariable Long id) {
+        Athlete athlete = athleteService.findById(id);
+        if (athlete == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(athlete);
+    }
+
+    @DeleteMapping("/athlete/{id}")
+    public ResponseEntity<Void> deleteAthlete(@PathVariable Long id) {
+        Athlete athlete = athleteService.findById(id);
+        if (athlete == null) {
+            return ResponseEntity.notFound().build();
+        }
+        athleteService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/athlete")
+    public ResponseEntity<Athlete> createAthlete(@RequestBody Athlete athlete) {
+        Athlete createdAthlete = athleteService.save(athlete);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdAthlete);
+    }
 }
